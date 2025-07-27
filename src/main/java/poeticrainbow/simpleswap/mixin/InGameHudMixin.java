750e1e52c426698390833352af31938d527465df
PoeticRainbow <jarrett.bayer@outlook.com>
2025-07-27 06:52:35 -0400

    Backport to 1.21.1

--- src/main/java/poeticrainbow/simpleswap/mixin/InGameHudMixin.java
+++ src/main/java/poeticrainbow/simpleswap/mixin/InGameHudMixin.java
@@ -13,8 +13,8 @@ public class InGameHudMixin {
     @Shadow private static final Identifier CROSSHAIR_TEXTURE = Identifier.ofVanilla("hud/crosshair");
 
     @ModifyArg(method = "renderCrosshair(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/render/RenderTickCounter;)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIII)V"),
-            index = 1)
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lnet/minecraft/util/Identifier;IIII)V"),
+            index = 0)
     private Identifier inject(Identifier sprite) {
         return SimpleSwap.SIMPLE_SWAP_KEY.isPressed() && sprite.equals(CROSSHAIR_TEXTURE) ?
                 Identifier.of("simpleswap", "blockswap") : sprite;
